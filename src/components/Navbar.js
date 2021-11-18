a6ae93f5197adce7c385b62a2cd9f67af84fcaa9
SantiagoGutierrezHernandez <86387559+SantiagoGutierrezHernandez@users.noreply.github.com>
2021-11-18 20:53:35 -0300

    Navbar agregada
    Agregado Bootstrap al HTML y una carpeta de componentes que contiene al Navbar.

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+function Navbar(){
+    return (
+    <nav className="nav-box d-none d-sm-flex black-background sticky-top flex-row align-items-center justify-content-between">
+        <img className="nav-item" style={{height: "45px", width: "auto", padding: "0"}} src="images/brand.png" alt=""/>
+        <a href="#inicio" className="nav-item text-center d-flex justify-content-center">
+            <span className="nav-text">Home</span>
+            <div className="nav-border-bottom"></div>
+        </a>
+        <a href="#sobre-nosotros" className="nav-item text-center d-flex justify-content-center">
+            <span className="nav-text">Sobre Nosotros</span>
+            <div className="nav-border-bottom"></div>
+        </a>
+        <a href="./html/productos.html" className="nav-item text-center d-flex justify-content-center">
+            <span className="nav-text">Nuestros Productos </span>
+            <div className="nav-border-bottom"></div>
+        </a>
+        <a href="#contacto" className="nav-item text-center d-flex justify-content-center">
+            <span className="nav-text">Contáctanos</span>
+            <div className="nav-border-bottom"></div>
+        </a>
+        <a href="./html/cart.html" className="nav-item text-center d-flex justify-content-center">
+            <span className="nav-text">Mi Carrito</span>
+            <div className="nav-border-bottom"></div>
+        </a>
+    </nav>
+    );
+}
+
+export default Navbar;
